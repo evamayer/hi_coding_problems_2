49c52df1536c91dd9d7fdbc5143cc0793f6baa3f
Eva Mayer <eva.mayer@hyperisland.se>
2022-11-28 16:11:12 +0100

    updated problem 1

--- CodingProblem_1.py
+++ CodingProblem_1.py
@@ -1,14 +1,11 @@
 from math import acos, sin, cos, radians
 
-def input_points():
-    print("Enter the names, latitudes and longitudes of two points on the Earth in degrees!")
-    name1 = input("Point one name: ")
-    lat_1 = radians(float(input("Point one latitude: ")))
-    long_1 = radians(float(input("Point one longitude: ")))
-    name2 = input("Point two name: ")
-    lat_2 = radians(float(input("Point two latitude: ")))
-    long_2 = radians(float(input("Point two longitude: ")))
-    return name1, lat_1, long_1, name2, lat_2, long_2
+def input_point():
+    name = input("Point name: ")
+    latitude = radians(float(input("Point latitude: ")))
+    longitude = radians(float(input("Point longitude: ")))
+    
+    return name, latitude, longitude
 
 def calc(lat_1, long_1, lat_2, long_2, speed):
     distance = 6371.01 * acos(sin(lat_1) * sin(lat_2) + cos(lat_1) * cos(lat_2) * cos(long_1-long_2))
@@ -20,7 +17,9 @@ def print_results(name1, name2, distance, duration, speed):
     print("A plane flying at", speed, "km/h would cover this distance in", round(duration, 2), "hours.")
 
 speed = 740
+print("Enter the names, latitudes and longitudes of two points on the Earth in degrees!")
 
-name1, lat_1, long_1, name2, lat_2, long_2 = input_points()
+name1, lat_1, long_1 = input_point()
+name2, lat_2, long_2 = input_point()
 distance, duration = calc(lat_1, long_1, lat_2, long_2, speed)
 print_results(name1, name2, distance, duration, speed)
